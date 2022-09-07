59e4964cfc4c3b3ab4ece385aa08e61ee97e2a1d
LyudmilaShch <111302180+LyudmilaShch@users.noreply.github.com>
2022-09-07 12:31:44 +0300

    04 - React кабзда как подробно PropsType

--- src/index.tsx
+++ src/index.tsx
@@ -8,12 +8,50 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+        <App />,
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// or send to an analytics endposint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
+
+/*let a = {
+    name: "Lyuda",
+    age: 10,
+    address: {
+        city: "SBP",
+        country: "Russia"
+    }
+};
+
+
+let users = [
+        {
+    name: "Lyuda",
+    age: 10,
+    address: {
+        city: "SBP",
+        country: "Russia"
+    }
+    },
+    {
+    name: "Viktor",
+    age: 13,
+    address: {
+        city: "SBP",
+        country: "Russia"
+    }
+    }
+]
+
+let b: Array<string | number>;
+b = [1, 2, 3, 4, 5];
+b = 5;
+b = ["Hello", "World", 5];
+b = [true, false];
+b = [];
+
+console.log(b);*/
