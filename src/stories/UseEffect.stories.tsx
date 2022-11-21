18f9eb79eba35b40d3df70c7ef3a4e47adda21a2
Lyudmila Shchepina <lyudashchepina1999@gmail.com>
2022-11-21 19:28:56 +0300

    22 - useEffect \ setTimeout \ setInterval

--- src/stories/UseEffect.stories.tsx
+++ src/stories/UseEffect.stories.tsx
@@ -36,3 +36,68 @@ export const SimpleExample = () => {
     </>
 
 }
+
+export const SetTimeOutExample = () => {
+    const [fake, setFake] = useState(1)
+    const [counter, setCounter] = useState(1)
+
+    console.log("SetTimeOutExample")
+
+
+    useEffect(() => {
+        setTimeout(() => {
+            console.log("setTimeOut")
+            document.title = counter.toString();
+        }, 1000)
+    }, [counter])
+
+
+    return <>
+        Hello, {counter} {fake}
+        <button onClick={() => setFake(fake + 1)}>fake +</button>
+        <button onClick={() => setCounter(counter + 1)}>counter +</button>
+    </>
+}
+
+export const SetIntervalExample = () => {
+    const [fake, setFake] = useState(1)
+    const [counter, setCounter] = useState(1)
+
+    console.log("SetIntervalExample")
+
+
+    useEffect(() => {
+        setInterval(() => {
+            console.log("setInterval " + counter)
+            setCounter((state) => state + 1)
+        }, 1000)
+    }, [])
+
+
+    return <>
+        Hello, Counter: {counter}, fake: {fake}
+    </>
+}
+
+export const ClockExample = () => {
+
+    const [date, setDate] = useState(new Date())
+    const seconds = date.getSeconds()
+    const minutes = date.getMinutes()
+    const house = date.getHours()
+
+
+    useEffect(() => {
+        console.log("ClockExample")
+        setInterval(() => {
+                setDate(new Date())
+                    , 1000
+            }
+        );
+    }, [])
+
+
+    return <>
+        {house}:{minutes}:{seconds}
+    </>
+}
